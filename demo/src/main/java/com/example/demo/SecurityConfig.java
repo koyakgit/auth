9995636d323b94d17a55ded7b7b69301cc8742de
koyakgit <keikotani926@gmail.com>
2020-06-01 06:59:25 +0900

    #3 サンプルコードをそのまま移植（まだコンパイルエラーあり）

--- demo/src/main/java/com/example/demo/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.filter.GenericFilterBean;
 
 /**
  * セキュリティ設定クラス
@@ -19,36 +28,97 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    /**
-     * セキュリティ設定
-     */
+    // @Autowired
+    // private UserRepository userRepository;
+
+    // ★1
+    @Value("${security.secret-key:secret}")
+    private String secretKey = "secret";
+
     @Override
-    public void configure(HttpSecurity security) throws Exception {
-        security.authorizeRequests()
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated()
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            // AUTHORIZE
+            .authorizeRequests()
+                .mvcMatchers("/hello/**")
+                    .permitAll()
+                .mvcMatchers("/user/**")
+                    .hasRole("USER")
+                .mvcMatchers("/admin/**")
+                    .hasRole("ADMIN")
+                .anyRequest()
+                    .authenticated()
             .and()
+            // EXCEPTION
+            .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint())
+                .accessDeniedHandler(accessDeniedHandler())
+            .and()
+            // LOGIN
             .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
+                .loginProcessingUrl("/login").permitAll()
+                    .usernameParameter("email")
+                    .passwordParameter("pass")
+                .successHandler(authenticationSuccessHandler())
+                .failureHandler(authenticationFailureHandler())
+            .and()
+            // ★2 LOGOUT
             .logout()
-                .permitAll();
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(logoutSuccessHandler())
+            .and()
+            // ★3 CSRF
+            .csrf()
+                .disable()
+            // ★4 AUTHORIZE
+            .addFilterBefore(tokenFilter(), UsernamePasswordAuthenticationFilter.class)
+            // ★5 SESSION
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            ;
+        // @formatter:on
     }
 
-    /**
-     * ユーザ情報取得サービスの定義
-     */
-    @Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-        final PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        final UserDetails user =
-			 User.withUsername("user")
-				.password(encoder.encode("password"))
-				.roles("USER")
-				.build();
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.eraseCredentials(true)
+                .userDetailsService(simpleUserDetailsService())
+                .passwordEncoder(passwordEncoder());
+    }
 
-		return new InMemoryUserDetailsManager(user);
-	}
+    @Bean("simpleUserDetailsService")
+    UserDetailsService simpleUserDetailsService() {
+        return new SimpleUserDetailsService(userRepository);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    GenericFilterBean tokenFilter() {
+        return new SimpleTokenFilter(userRepository, secretKey);
+    }
+
+    AuthenticationEntryPoint authenticationEntryPoint() {
+        return new SimpleAuthenticationEntryPoint();
+    }
+
+    AccessDeniedHandler accessDeniedHandler() {
+        return new SimpleAccessDeniedHandler();
+    }
+
+    AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new SimpleAuthenticationSuccessHandler(secretKey);
+    }
+
+    AuthenticationFailureHandler authenticationFailureHandler() {
+        return new SimpleAuthenticationFailureHandler();
+    }
+
+    LogoutSuccessHandler logoutSuccessHandler() {
+        return new HttpStatusReturningLogoutSuccessHandler();
+    }
+
 }
